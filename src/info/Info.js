4c3da1474a7fca69289c235d8c0401dfdb4deb9d
Sean Currlin <sean.currlin@gmail.com>
2023-08-14 07:11:50 -0400

    update portfolio projects

--- src/info/Info.js
+++ src/info/Info.js
@@ -2,13 +2,16 @@ import self from "../img/self.png"
 import mock1 from "../img/mock1.png"
 import mock2 from "../img/mock2.png"
 import mock3 from "../img/mock3.png"
-import mock4 from "../img/mock4.png"
 import mock5 from "../img/mock5.png"
+import mock6 from "../img/mock6.png"
+import mock7 from "../img/mock7.png"
+
+import { Class } from "node-forge/lib/asn1"
 
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
 
-export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
+export let colors = ["rgb(9,175,236)", "rgb(18,103,237)"];
 /*
 I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
 These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
@@ -23,118 +26,118 @@ of what each of the values mean.
  */
 
 export const info = {
-    firstName: "John",
-    lastName: "Smith",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
+    firstName: "Sean",
+    lastName: "Currlin",
+    initials: "sc", // the example uses first and last, but feel free to use three or more if you like.
     position: "a Full Stack Developer",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
     baseColor: colors[0],
     miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
         {
-            emoji: '☕',
-            text: 'fueled by coffee'
-        },
-        {
             emoji: '🌎',
-            text: 'based in the US'
+            text: 'Based in Boston, MA'
         },
         {
-            emoji: "💼",
-            text: "Systems Engineer at Google"
+            emoji: "🎹",
+            text: "Professional Musician"
         },
         {
             emoji: "📧",
-            text: "johnsmith@gmail.com"
+            text: "sean.currlin@gmail.com"
+        },
+        {
+            emoji: "📂",
+            text: <a href="https://drive.google.com/file/d/1Yf-tfU1pl9ber3q5hc3cgpCjybwlC-my/view?usp=drive_link" download="Sean_Currlin_SE_Resume.pdf">Resume</a>
         }
     ],
     socials: [
         {
-            link: "https://facebook.com",
-            icon: 'fa fa-facebook',
-            label: 'facebook'
-        },
-        {
-            link: "https://instagram.com",
-            icon: 'fa fa-instagram',
-            label: 'instagram'
-        },
-        {
-            link: "https://github.com",
+            link: "https://github.com/Scurrlin",
             icon: "fa fa-github",
             label: 'github'
         },
         {
-            link: "https://linkedin.com",
+            link: "https://www.linkedin.com/in/seancurrlin/",
             icon: "fa fa-linkedin",
             label: 'linkedin'
         },
         {
-            link: "https://twitter.com",
-            icon: "fa fa-twitter",
-            label: 'twitter'
+            link: "https://open.spotify.com/artist/1yes1OaMmV9w58iMeggawH",
+            icon: "fa fa-spotify",
+            label: 'spotify'
         }
 // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
 // Just change the links so that they lead to your social profiles.
 
     ],
-    bio: "Hello! I'm John. I'm a systems engineer for Google. I studied CompSci at Harvard, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+    bio: "I'm a passionate problem solver with a lifelong interest in software, tech hardware and music. Skilled in critical thinking and finding creative solutions, I have extensive experience solving complex problems from my time as a classical musician. I approach software development projects with enthusiasm and unwavering commitment to finding the best possible solution.",
     skills:
         {
-            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma'],
-            exposedTo: ['nodejs', 'python', 'adobe illustrator']
+            proficientWith: ['Javascript', 'React', 'Git', 'Github', 'Bootstrap', 'HTML5', 'CSS3', 'Express', 'Mongoose', 'MongoDB', 'NodeJS', 'Amazon S3'],
+            exposedTo: ['Django', 'Python', 'PostgreSQL']
         }
     ,
     hobbies: [
         {
-            label: 'reading',
-            emoji: '📖'
+            label: 'Hardware Upgrades',
+            emoji: '🖥'
         },
         {
-            label: 'theater',
-            emoji: '🎭'
+            label: 'Music',
+            emoji: '🎧'
         },
         {
-            label: 'movies',
-            emoji: '🎥'
+            label: 'Fitness',
+            emoji: '💪🏻'
         },
         {
-            label: 'cooking',
-            emoji: '🌶'
+            label: 'Wrtiting',
+            emoji: '✍🏻'
+        },
+        {
+            label: 'Gaming',
+            emoji: '🎮'
         }
 // Same as above, change the emojis to match / relate to your hobbies or interests.
 // You can also remove the emojis if you'd like, I just think they look cute :P
     ],
     portfolio: [ // This is where your portfolio projects will be detailed
         {
-            title: "Project 1",
+            title: "🏋️ Lift Off 🚀 (MERN Stack)",
             live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
             source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
             image: mock1
         },
         {
-            title: "Project 2",
+            title: "Dev Blog (MEN Stack)",
             live: "https://paytonpierce.dev",
             source: "https://github.com/paytonjewell",
             image: mock2
         },
         {
-            title: "Project 3",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
+            title: "Interval Training Game",
+            live: "https://hearos-journey.onrender.com",
+            source: "https://github.com/Scurrlin/Interval-Training-Game",
             image: mock3
         },
         {
-            title: "Project 4",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
+            title: "JS Calculator",
+            live: "https://js-calculator-hurx.onrender.com",
+            source: "https://github.com/Scurrlin/JS-Calculator",
             image: mock4
         },
         {
-            title: "Project 5",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
+            title: "Hangman",
+            live: "https://spaceman-hsvf.onrender.com",
+            source: "https://github.com/Scurrlin/S_P_A_C_E_M_A_N",
             image: mock5
+        },
+        {
+            title: "Rock Paper Scissors",
+            live: "https://rps-2s17.onrender.com",
+            source: "https://github.com/Scurrlin/Rock-x-Paper-x-Scissors",
+            image: mock6
         }
     ]
 }
