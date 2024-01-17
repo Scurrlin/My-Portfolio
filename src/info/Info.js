94fe299533b45bfc13c4dd3cac7c982009d81ad9
Sean Currlin <sean.currlin@gmail.com>
2024-01-16 20:22:32 -0500

    update resume with link to 3D portfolio

--- src/info/Info.js
+++ src/info/Info.js
@@ -46,7 +46,7 @@ export const info = {
         },
         {
             emoji: "📂",
-            text: <a href="https://drive.google.com/file/d/1SPYBI9dcrj2s8i5SaKN-g2uWbAU3aEwU/view?usp=sharing"download="Sean Currlin - Software Engineer.pdf">Resume</a>
+            text: <a href="https://drive.google.com/file/d/13pVyDzLf_SrhE5HNTrm2dhCg_n4eMleD/view?usp=sharing"download="Sean Currlin - Software Engineer.pdf">Resume</a>
         }
     ],
     socials: [
